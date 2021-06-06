feb2164091233d24995836943cfa838597757135
tallyshe <karhoshe@gmail.com>
2021-06-06 17:13:45 +0800

    Add ShowAddTask 1.Header onADD and showAdd work correctly 2.addTask work as intent

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(true)
   const [tasks, setTasks] = useState([
     {
         "id": 1,
@@ -26,6 +27,12 @@ function App() {
   ])
 
   // Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
   
 
   // delete Task
@@ -44,8 +51,11 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask />
+      <Header 
+      onAdd={() => setShowAddTask(!showAddTask)}
+      showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
        ) : (
